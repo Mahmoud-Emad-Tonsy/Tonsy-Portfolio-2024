163e8cd1aa1d3f7b6c6b8882826d915b8f7bb619
MahmoudTonsy <m_tonsy@yahoo.com>
2024-05-11 14:23:49 +0300

    change landing img

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,5 +1,5 @@
 import resume from '../assets/pdf/Mahmoud_Tonsy_Cv(2023).pdf'
-import landingImg from "../assets/png/ladning1.jpeg"
+import landingImg from "../assets/png/ladning.png"
 export const headerData = {
   name: "Mahmoud Tonsy",
   title: "Full Stack Developer",
