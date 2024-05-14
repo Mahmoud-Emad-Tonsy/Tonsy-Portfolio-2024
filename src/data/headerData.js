a6bfad71fed3e25fa42a2973b8b328000be70335
MahmoudTonsy <m_tonsy@yahoo.com>
2024-05-14 21:17:38 +0300

    update cv

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,8 +1,8 @@
-import resume from '../assets/pdf/Mahmoud_Tonsy_Cv(2023).pdf'
+import resume from '../assets/pdf/Mahmoud Emad Tonsy (FrontEnd Developer-Cv-5-2024)s.pdf'
 import landingImg from "../assets/png/ladning1.jpeg"
 export const headerData = {
   name: "Mahmoud Tonsy",
-  title: "Full Stack Developer",
+  title: "Front End Developer",
   desciption:
     "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
   image: landingImg,
